19cdb4c4dba2292e352d50cfa0abfc2ade82c165
Danijel Lakic <danijellakic@yahoo.com>
2022-12-06 13:10:30 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Featured from "./components/Featured"
 import Converter from "./components/Converter.jsx"
-import News from "./components/News";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
     <Hero/>
     <Featured/>
     <Converter/>
-    <News/>
+    <Footer/>
 
     </>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="container">
             <div className="left">
                 <p>With Currency Value you can check live currency data and view currency value.</p>
-                <h1>Here you find rates for different currencies and can compare the rates for dollars, Euros or Punds to the Swedish crona.</h1>
+                <h1>Here you find rates for different currencies and compare the rates for dollars, euros and more.</h1>
                 <p>Check live foreign currency exchange rates.</p>
                 <div className="input-container">
                     <input type="email" placeholder='Enter your email' />
